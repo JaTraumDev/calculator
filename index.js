3af464e7592a90c11008cd7c65953010b15c98a5
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-25 18:02:22 +0100

    feat: add calculation input with keyboard

--- index.js
+++ index.js
@@ -12,24 +12,57 @@ const multiply = function (a, b) {
 
 const divide = function (a, b) {
     if (b === 0) {
-        return "Pfffff...";
+        return null;
     } else {
         return a / b;
     }
 };
 
 const clear = function () {
-    calc.left = null;
-    calc.operator = null;
-    calc.right = null;
+    calc.left = "";
+    calc.operator = "";
+    calc.right = "";
 };
 
 let calc = {
-    left: null,
-    operator: null,
-    right: null,
+    left: "",
+    operator: "",
+    right: "",
 };
+
+function setCalculationInput() {
+    calculation.textContent = `${calc.left} ${calc.operator} ${calc.right}`;
+}
+
+function handleBackSpace() {
+    if (calc.right) {
+        calc.right = calc.right.slice(0, -1);
+    } else if (calc.operator) {
+        calc.operator = "";
+    } else if (calc.left) {
+        calc.left = calc.left.slice(0, -1);
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key >= "0" && e.key <= "9") {
+        if (!calc.operator) {
+            calc.left += e.key;
+        } else {
+            calc.right += e.key;
+        }
+    } else if (e.key.includes("+", "-", "*", "/")) {
+        calc.operator = e.key;
+    } else if (e.key === "Backspace") {
+        handleBackSpace();
+    }
+    setCalculationInput();
+});
+
+const equals = document.querySelector("#equals");
+
+equals.addEventListener("click", () => {});
 
 const calculation = document.querySelector("#calculation");
 
-calculation.textContent = `${calc.left} ${calc.operator} ${calc.right}`;
+setCalculationInput();
