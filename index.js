5178ea040974cdb0ae91d64f9400539e90f4711e
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-25 16:31:58 +0100

    feat: add core math functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const add = function (a, b) {
+    return a + b;
+};
 
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    if (b === 0) {
+        return "Pfffff...";
+    } else {
+        return a / b;
+    }
+};
