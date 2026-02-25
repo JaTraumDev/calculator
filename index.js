5daeb469ffb75badc2358698e53be1f6e28af00f
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-25 18:51:14 +0100

    feat: add hasCalculated flag to prevent result mutation

--- index.js
+++ index.js
@@ -12,22 +12,10 @@ const multiply = function (a, b) {
 
 const divide = function (a, b) {
     if (b === 0) {
-        return null;
+        calculation.textContent = "ERROR";
     } else {
-        return a / b;
+        return (a / b).toFixed(2);
     }
-};
-
-const clear = function () {
-    calc.left = "";
-    calc.operator = "";
-    calc.right = "";
-};
-
-let calc = {
-    left: "",
-    operator: "",
-    right: "",
 };
 
 function setCalculationInput() {
@@ -44,25 +32,86 @@ function handleBackSpace() {
     }
 }
 
+const clear = function () {
+    calc.left = "";
+    calc.operator = "";
+    calc.right = "";
+};
+
+function setOperator(e) {
+    if (e.key === "*") {
+        calc.operator = "x";
+    } else if (e.key === "/") {
+        calc.operator = "÷";
+    } else {
+        calc.operator = e.key;
+    }
+}
+
+let calc = {
+    left: "",
+    operator: "",
+    right: "",
+    calculate() {
+        hasCalculated = true;
+        if (this.left && !this.right) {
+            this.operator = "";
+            hasCalculated = false;
+        } else {
+            if (this.operator === "+") {
+                this.left = add(parseFloat(this.left), parseFloat(this.right));
+                this.operator = "";
+                this.right = "";
+            } else if (this.operator === "-") {
+                this.left = subtract(parseFloat(this.left), parseFloat(this.right));
+                this.operator = "";
+                this.right = "";
+            } else if (this.operator === "÷") {
+                this.left = divide(parseFloat(this.left), parseFloat(this.right));
+                this.operator = "";
+                this.right = "";
+            } else if (this.operator === "x") {
+                this.left = multiply(parseFloat(this.left), parseFloat(this.right));
+                this.operator = "";
+                this.right = "";
+            }
+        }
+
+        setCalculationInput();
+    },
+};
+
 document.addEventListener("keydown", (e) => {
+    const operators = ["+", "-", "*", "/", "x"];
+
     if (e.key >= "0" && e.key <= "9") {
         if (!calc.operator) {
+            if (hasCalculated === true && !operators.includes(e.key)) {
+                clear();
+                hasCalculated = false;
+            }
             calc.left += e.key;
         } else {
             calc.right += e.key;
         }
-    } else if (e.key.includes("+", "-", "*", "/")) {
-        calc.operator = e.key;
+    } else if (operators.includes(e.key) && !calc.right) {
+        setOperator(e);
+    } else if (operators.includes(e.key) && calc.right) {
+        calc.calculate();
+        setOperator(e);
     } else if (e.key === "Backspace") {
         handleBackSpace();
+    } else if (e.key === "Enter" || e.key === "=") {
+        calc.calculate();
     }
+
     setCalculationInput();
 });
+
+let hasCalculated = false;
 
 const equals = document.querySelector("#equals");
 
 equals.addEventListener("click", () => {});
 
 const calculation = document.querySelector("#calculation");
-
-setCalculationInput();
